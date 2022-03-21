a921c3ed4f89f6acb2172a447f5f7b753d2c6339
your name <huong.nmd.61kt@ntu.edu.vn>
2022-03-21 18:20:22 +0700

    VDtrenlop
    Signed-off-by: your name <huong.nmd.61kt@ntu.edu.vn>

---
+++ TCP/VD1.java
@@ -0,0 +1,20 @@
+package TCP;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class VD1 {
+
+	public static void main(String[] args) {
+		try {
+			Socket soc = new Socket("ntu.edu.vn", 80);
+			System.out.print("Connect!..");
+		} catch (IOException e) {
+
+			System.out.print("NoConnect....");
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ Web/CT2.java
@@ -0,0 +1,41 @@
+package Web;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class CT2 {
+
+	public static void main(String[] args) throws IOException {
+		String chuoiURL="https://www.ntu.edu.vn/Gioi-thieu/Thong-điep-cua-Hieu-truong";
+		//
+		URL u = new URL(chuoiURL);
+		//Mở kết nối
+		URLConnection uCon = u.openConnection();
+		uCon.connect();
+		
+		
+		// Lấy về luồng nhập
+		InputStream in = uCon.getInputStream();
+		InputStreamReader inR = new InputStreamReader(in);
+		BufferedReader buff = new BufferedReader(inR);
+		//đoc buffer để in ra màn hình
+		String lineHTML;
+		while(true)
+		{
+			lineHTML = buff.readLine();
+			if(lineHTML==null) break;
+			else System.out.println(lineHTML);
+		}
+		
+		buff.close();
+		inR.close();
+		in.close();
+
+	}
+
+}
